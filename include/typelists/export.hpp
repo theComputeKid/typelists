c903020c63e4b3f13aa9b538e30be072e3c9b4b4
theComputeKid <theComputeKid@gmail.com>
2022-06-10 22:39:09 +0100

    Rename instantiation macro.

--- include/typelists/export.hpp
+++ include/typelists/export.hpp
@@ -21,13 +21,13 @@
  * @arg func - function name to be instantiated. Must not be namespaced.
  * @arg typelist - types to instatiate.
  */
-#define TEMPLATE_INSTANTIATE_SINGLE_TEMPLATE(func, typelist)                                                           \
+#define TYPE_LIST_INSTANTIATE_1_TEMPLATE(func, typelist)                                                               \
   std::any func##TemplateInstantiate(typelist const types)                                                             \
   {                                                                                                                    \
     return std::visit([](auto t) { return std::any(func<decltype(t)>); }, types);                                      \
   }
 
-#define TEMPLATE_INSTANTIATE_DOUBLE_TEMPLATE(func, typelist1, typelist2)                                               \
+#define TYPE_LIST_INSTANTIATE_2_TEMPLATE(func, typelist1, typelist2)                                                   \
   std::any func##TemplateInstantiate(typelist1 const types1, typelist2 const types2)                                   \
   {                                                                                                                    \
     return std::visit([](auto t1, auto t2) { return std::any(func<decltype(t1), decltype(t2)>); }, types1, types2);    \
--- test/export-func.cpp
+++ test/export-func.cpp
@@ -4,10 +4,10 @@ template <typelists::base::real::concepts::All T1, typename T2>
 void doubleTemplateFunc()
 {
 }
-TEMPLATE_INSTANTIATE_DOUBLE_TEMPLATE(doubleTemplateFunc, typelists::base::real::All, typelists::base::real::All)
+TYPE_LIST_INSTANTIATE_2_TEMPLATE(doubleTemplateFunc, typelists::base::real::All, typelists::base::real::All)
 
 template <typelists::base::real::concepts::Float T>
 void singleTemplateFunc()
 {
 }
-TEMPLATE_INSTANTIATE_SINGLE_TEMPLATE(singleTemplateFunc, typelists::base::real::Float)
+TYPE_LIST_INSTANTIATE_1_TEMPLATE(singleTemplateFunc, typelists::base::real::Float)
